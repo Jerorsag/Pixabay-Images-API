757d01362ef2ec9eb6d325660bcdc999f95b2345
JeroRodriguez <jeronimoroseag@gmail.com>
2025-04-07 14:56:51 -0500

    Added Async Await

--- js/app.js
+++ js/app.js
@@ -45,19 +45,28 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
     const key = '47009158-05b4300166af3d431c14893e7';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url) 
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    // fetch(url) 
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     });
+
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
